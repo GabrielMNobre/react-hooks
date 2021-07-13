8457dd32cded25890e728e677a5049e98fa08993
GabrielMNobre <gmn.nobre@gmail.com>
2021-07-13 16:35:37 -0300

    Adicionando novas interações com o UseReducer

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,11 +1,32 @@
-import React, { useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
 
 const initialState = {
   cart: [],
   user: null,
   number: 0,
 };
+
+const challengeInitialState = {
+  number: 0,
+  result: 0,
+};
+
+function challenge(state, action) {
+  switch (action.type) {
+    case 'number_add7':
+      return { ...state, number: state.number + 7 };
+    case 'number_add':
+      return { ...state, number: state.number + parseInt(action.payload.number, 10) };
+    case 'number_dividedBy25':
+      return { ...state, number: state.number / 25 };
+    case 'result':
+      return { ...state, result: Math.ceil(state.number) };
+    default:
+      return state;
+  }
+}
 
 // função de evolução do estado inicial
 function reducer(state, action) {
@@ -21,13 +42,22 @@ function reducer(state, action) {
 
 function UseReducer() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [challengeState, challengeDispatch] = useReducer(challenge, challengeInitialState);
   const [login, setLogin] = useState('');
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    challengeDispatch({ type: 'result' });
+  }, [challengeState.number]);
 
   return (
     <div className="UseReducer">
       <PageTitle
         title="Hook UseReducer"
         subtitle="Uma outra forma de ter estado em componentes funcionais!"
+      />
+      <SectionTitle
+        title="Exercício #01 - Primeiro contato com UseReducer"
       />
       <div className="center">
         {
@@ -57,6 +87,47 @@ function UseReducer() {
             onClick={() => dispatch({ type: 'login', payload: login })}
           >
             LOGIN
+          </button>
+        </div>
+      </div>
+      <SectionTitle
+        title="Desafio UseReducer"
+      />
+      <div className="center">
+        <span className="text">{ challengeState.result }</span>
+        <div>
+          <button
+            type="button"
+            className="btn"
+            onClick={() => challengeDispatch({ type: 'number_add7' })}
+          >
+            +7
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={() => challengeDispatch({ type: 'number_dividedBy25' })}
+          >
+            &divide;25
+          </button>
+        </div>
+        <div>
+          <input
+            type="number"
+            className="input"
+            onChange={(e) => setNumber(e.target.value)}
+          />
+          <button
+            type="button"
+            className="btn"
+            onClick={() => challengeDispatch({
+              type: 'number_add',
+              payload: {
+                number,
+              },
+            })}
+          >
+            +
           </button>
         </div>
       </div>
